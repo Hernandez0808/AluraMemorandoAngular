6e28b53fbf61a86f2f53dfcef122a1976b9a172e
Daniel Hernandez <daniel0808soares@gmail.com>
2023-10-14 16:40:20 -0300

    4 - Animando formulario e elemts não anxdo ao DOM

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -14,4 +14,59 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     })),
     animate(200)
   ])
-])
+]);
+
+export const showStateTrigger = trigger('shownState', [
+    transition(':enter', [
+        style({
+            opacity:0
+        }),
+        animate(300, style({
+            opacity:1
+        }))
+    ]),
+    transition(':leave',[
+        animate(300,style({
+            opacity:0
+        }))
+    ])
+]);
+
+
+//aplicando animação em elementos não anexados ao dom 
+// export const showStateTrigger = trigger('shownState', [
+//     state('shown', style({
+
+//     })),
+//     transition('void => shown', [
+//         style({
+//             opacity:0
+//         }),
+//         animate(300, style({
+//             opacity:1
+//         }))
+//     ]),
+//     transition('shown => void',[
+//         animate(300,style({
+//             opacity:0
+//         }))
+//     ])
+// ]);
+
+
+//aplicando animação em elementos não anexados ao dom com estado void "coringa"
+// export const showStateTrigger = trigger('shownState', [
+//     transition('void => *', [
+//         style({
+//             opacity:0
+//         }),
+//         animate(300, style({
+//             opacity:1
+//         }))
+//     ]),
+//     transition('* => void',[
+//         animate(300,style({
+//             opacity:0
+//         }))
+//     ])
+// ]);
